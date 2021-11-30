e5901ee300cafb0217ecd1260d86661541949bb8
unknown <eulziisaikhan@miu.edu>
2021-11-29 20:12:11 -0600

    lab1-3 fix 2

--- lab3-af/lab3-af/src/main/java/lab3/lab3af/service/UserServiceImpl.java
+++ lab3-af/lab3-af/src/main/java/lab3/lab3af/service/UserServiceImpl.java
@@ -11,7 +11,8 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
 
-    private List<User> users = new ArrayList<>();
+    //private List<User> users = new ArrayList<>();
+    boolean flag=false;
 
     @Autowired
     UserRepo userRepo;
@@ -19,15 +20,17 @@ public class UserServiceImpl implements UserService {
     private void createFake(){
         User u1 = new User("123", "John", "Brown", "test1@gmail.com", "pass123", 75);
         User u2 = new User("124", "Anthony", "Legend", "test2@gmail.com", "pass123", 85);
-        users.add(u1);
-        users.add(u2);
+        userRepo.save(u1);
+        userRepo.save(u2);
     }
 
     @Override
     public List<User> getAll() {
-        if (users.size()==0){
+        if (flag==false){
             createFake();
+            flag=true;
         }
+        List<User> users = new ArrayList<>();
 
         var iterable = userRepo.findAll();
         for (User u: iterable){
@@ -40,13 +43,13 @@ public class UserServiceImpl implements UserService {
     @Override
     public void save(User u) {
         userRepo.save(u);
-
     }
 
     @Override
     public List<String[]> getBasic() {
-        if (users.size()==0){
+        if (flag==false){
             createFake();
+            flag=true;
         }
 
         List<String[]> basics = new ArrayList<String[]>();
